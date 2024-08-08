ef165f31bfc893c22fc9303088908f74256f2d66
Vulth01 <damienharwood14@gmail.com>
2024-08-08 15:04:42 +0200

    Added Foo.lisp
    An example script

---
+++ foo.lisp
@@ -0,0 +1,20 @@
+(defun Square (x)
+  (* x x))
+(defun Multiply(x y)
+  (* x y))
+
+(defun Divide (x y)
+  (/ x y))
+
+(defun Add (x y)
+  (+ x y))
+
+(defun Subtract (x y)
+  (- x y))
+
+(defun Modulo (x y)
+  (mod x y))
+
+(defun Bamboozle (x)
+  (Square x)
+  )
